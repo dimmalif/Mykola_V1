654b6caf9eea95ea235e29760ee8076998720b4b
Derix6669 <BENDER03052@gmail.com>
2022-12-14 15:51:07 +0200

    YT_music

--- test_script/Test.py
+++ test_script/Test.py
@@ -1,20 +1,22 @@
+import webbrowser
 from ytmusicapi import YTMusic
-import youtube_dl
 
-req = 'заточка'
+req = 'дзідзьо'
 
 
-def search_music(req):
+def search_music(requirements):
     yt = YTMusic()
-    search_results = yt.search(req ,filter='albums')
-    albums = YTMusic.get_album( browseId='MPREb_L0iCMX09J83')
-    print(albums)
-    res = []
-    for i in search_results:
-        res.append(i)
-        print(res)
-    return
+    search_results = yt.search(requirements, filter='albums')
+
+    ids = [search_results[i]['browseId'] for i in range(len(search_results))]
+
+    albums = [yt.get_album(browseId=ids[i]) for i in range(len(ids))]
+
+    audio_playlist_id = [i['audioPlaylistId'] for i in albums]
+
+    link = f'https://music.youtube.com/watch?v=&list={audio_playlist_id[0]}'
+
+    webbrowser.open(link)
 
 
 search_music(req)
-
