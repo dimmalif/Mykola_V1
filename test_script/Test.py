af8169b4cfb414af7ba67ab8f7545fd3922382dc
Dmytro <dimmalif@gmail.com>
2022-12-15 14:30:51 +0200

    downloads_криво_працює

--- test_script/Test.py
+++ test_script/Test.py
@@ -5,7 +5,7 @@ from ytmusicapi import YTMusic
 download_path = 'C:/Users/Dmytro/Desktop/Mykola_V1/bin/test_script'
 download_file = 'C:/Users/Dmytro/Desktop/Mykola_V1/bin/test_script/Failed.txt'
 
-req = 'ThePrettyReckless'
+req = 'The pretty rekless '
 
 
 # region trash class
@@ -42,46 +42,45 @@ req = 'ThePrettyReckless'
 def search_music_albums(requests):
     all_links = []
     yt = YTMusic()
-    search_results = yt.search(requests, filter='albums')
-    ids = [search_results[i]['browseId'] for i in range(len(search_results))]
+    search_results_albums = yt.search(requests, filter='albums')
+    ids = [search_results_albums[i]['browseId'] for i in range(len(search_results_albums))]
     albums = [yt.get_album(browseId=ids[i]) for i in range(len(ids))]
-    print(albums)
+    #print(albums)
     audio_playlist_id = [i['audioPlaylistId'] for i in albums]
+    first_links = f'https://music.youtube.com/watch?v=&list={audio_playlist_id[0]}'
 
-#region трохи говнокоду
+    # region трохи говнокоду
     lst = [i for i in range(len(audio_playlist_id))]
     audio_playlist_dictionary = dict(zip(audio_playlist_id, lst))
-    print(audio_playlist_dictionary)
-
-    first_links = f'https://music.youtube.com/watch?v=&list={audio_playlist_id[0]}'
+    #print(audio_playlist_dictionary)
 
     for i in audio_playlist_dictionary:
         all_links.append(f'https://music.youtube.com/watch?v=&list={audio_playlist_dictionary[i]}')
-    print(all_links)
-#endregion
+    #print(all_links)
+    # endregion
 
-    webbrowser.open(first_links)
-    print(first_links)
+    #webbrowser.open(first_links)
+    #print(first_links)
     return first_links
 
 
 def run(link):
     video_info = youtube_dl.YoutubeDL().extract_info(url=link, download=False)
     filename = f"{video_info['title']}.mp3"
+    print(video_info['title'])
     options = {
         'format': 'bestaudio/best',
-        'keepvideo': False,
-        'outtmpl': filename,
+        'ffmpeg': True
     }
 
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download([video_info['webpage_url']])
+    #with youtube_dl.YoutubeDL(options) as ydl:
+        #ydl.download([video_info['webpage_url']])
 
-    print("Download complete... {}".format(filename))
+    #print("Download complete... {}".format(filename))
 
 
 # endregion
-search_music_albums(req)
+run(search_music_albums(req))
 
 # region trash download
 # def download_sound(links):
